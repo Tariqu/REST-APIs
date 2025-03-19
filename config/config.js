b1287d34c20228920b6940eb2cf793ae1028cb4b
Ahmad Hassan Khan <98497427+mrahmadhassankhan@users.noreply.github.com>
2025-03-19 11:00:02 +0500

    fix: fixed the config for test

--- config/config.js
+++ config/config.js
@@ -11,11 +11,13 @@ module.exports = {
         seederStorage: 'sequelize',
     },
     test: {
-        username: 'root',
-        password: null,
-        database: 'database_test',
-        host: '127.0.0.1',
-        dialect: 'mysql',
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: 'postgres',
+        seederStorage: 'sequelize',
     },
     production: {
         username: process.env.DB_USERNAME,
